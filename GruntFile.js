1fbd8ea812ff1b37e15f1cfe2362be57d9ac2675
David Hamilton <nova706@gmail.com>
2015-03-23 21:58:12 -0600

    Add Grunt and uglify and remove demo app from master branch

---
+++ GruntFile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            min: {
+                options: {
+                    mangle: true,
+                    sourceMap: true,
+                    sourceMapName: 'dist/recall.min.map'
+                },
+                files: {
+                    'dist/recall.min.js': ['src/**/*.js']
+                }
+            },
+            concat: {
+                options: {
+                    mangle: false,
+                    beautify: true,
+                    compress: false,
+                    preserveComments: true
+                },
+                files: {
+                    'dist/recall.js': ['src/**/*.js']
+                }
+            }
+        },
+        karma: {
+            main: {
+                configFile: './karma.conf.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-karma');
+
+    grunt.registerTask('default', ['uglify', 'karma']);
+};
